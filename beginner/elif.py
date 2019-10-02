b644c2b5df52314c4ecf308f6daab54b195906f3
Graham Hewett <hewettgraham@gmail.com>
2019-10-03 00:39:14 +0100

    complete 3 elif questions

--- beginner/elif.py
+++ beginner/elif.py
@@ -1,20 +1,33 @@
-def smallestNum(a, b):
+def truthy_or_falsy(value):
+	if value:
+		return 'truthy'
+	return 'falsey'
+print(truthy_or_falsy(0b110))
+#falsey values are False, None, 0, '', [], (), {}
+
+def smallest_num(a, b):
 	if a < b:
 		return 'The smallest number is ' + str(a)
 	return 'The smallest number is ' + str(b)
-print(smallestNum(0b110, 55))
+print(smallest_num(0b110, 55))
 
-def OddOrEven(num):
+def odd_or_even(num):
 	if num % 2 == 0:
 		return 'even'
 	return 'odd'
 #hint use the modulus operator %
-print(OddOrEven(0xe1b))
+print(odd_or_even(0o71))
 
-def equality(truthy):
-	if truthy:
-		return 'truthy'
-	return 'falsey'
-print(equality(0b110))
+def check_involving_or(value):
+	if "condition 1" or "condition 2":
+		return 'one of these conditions is true'
+	return 'both of these conditions evaluate to false'
 
-#falsey values are False, None, 0, '', [], (), {}
+def check_if_positive_num(value):
+	if type(value) == '?number?' and value > 0:
+		return ''
+	elif type(value) != '?number?':
+		return ''
+	return ''
+#hint use the modulus operator %
+print(odd_or_even(0o71))
--- beginner/test_elif.py
+++ beginner/test_elif.py
@@ -1,21 +1,89 @@
-from if_else import odd_or_even
-from if_else import smallest_num
-# from if_else import odd_or_even
-# from if_else import odd_or_even
-# from if_else import odd_or_even
+from if_else import odd_or_even, truthy_or_falsy, smallest_num, _and, _or
 
-def test_odd_or_even():
-	a, b, c, d, e = 0, 7, -8, 100, -99
-	assert OddOrEven(a) == 'even'
-	assert OddOrEven(b) == 'odd'
-	assert OddOrEven(c) == 'even'	
-	assert OddOrEven(d) == 'even'	
-	assert OddOrEven(e) == 'odd'	
+class TestUM(unittest.TestCase):
+    def test_truthy_or_falsy(self):
+	self.assertEqual(truthy_or_falsy(0),
+	"falsy")
+	self.assertEqual(truthy_or_falsy(-13),
+	"truthy")
+	self.assertEqual(truthy_or_falsy(0b000),
+	"falsy")
+	self.assertEqual(truthy_or_falsy(0x101),
+	"truthy")
 
-def test_smallest_num():
-	x, y = 0b1101, -900
-	a, b = 0xef40, -0b111
-	assert smallestNum(2, -1) == 'The smallest number is -1'
-	assert smallestNum(2, 3) == 'The smallest number is 2'
-	assert smallestNum(0b111, -0xef4) == 'The smallest number is -3828'
-	assert smallestNum(0b110, 55) == 'The smallest number is 6'
+class TestUM(unittest.TestCase):
+    def test_truthy_or_falsy(self):
+	self.assertEqual(truthy_or_falsy(''),
+	"Falsy")
+	self.assertEqual(truthy_or_falsy("false"),
+	"Truthy")
+	self.assertEqual(truthy_or_falsy("""-##!"""),
+	"Truthy")
+
+class TestUM(unittest.TestCase):
+    def test_truthy_or_falsy(self):
+	self.assertEqual(truthy_or_falsy([1,2,3]), 
+	"Truthy")
+	self.assertEqual(truthy_or_falsy([]), 
+	"Falsy")
+	self.assertEqual(truthy_or_falsy([0, None, {}]), 
+	"Truthy")
+
+class TestUM(unittest.TestCase):
+    def test_truthy_or_falsy(self):
+	self.assertEqual(truthy_or_falsy({}),
+	"Falsy")
+	self.assertEqual(truthy_or_falsy({"isfalse": False}),
+	"Truthy")
+
+class TestUM(unittest.TestCase):
+    def test_odd_or_even(self):
+	self.assertEqual(odd_or_even(-5),"Odd")
+	self.assertEqual(odd_or_even(22), "Even")
+	self.assertEqual(odd_or_even(0), "Even")
+
+class TestUM(unittest.TestCase):
+    def test_odd_or_even(self):
+	self.assertEqual(odd_or_even(-0o776),"Even")
+	self.assertEqual(odd_or_even(-0b110), "Even")
+	self.assertEqual(odd_or_even(0x111), "Odd")
+
+class TestUM(unittest.TestCase):
+    def test_smallest_num(self):
+	self.assertEqual(smallest_num(-2, 3),
+	"The smallest number is -2")
+	self.assertEqual(smallest_num(15, 5),
+	"The smallest number is 5")
+
+class TestUM(unittest.TestCase):
+    def test_smallest_num(self):
+	self.assertEqual(smallest_num(0o765, 0x11),
+	"The smallest number is 17")
+	self.assertEqual(smallest_num(0b110, 55),
+	"The smallest number is 6")
+
+class TestUM(unittest.TestCase):
+    def test_check_if_positive_num(self):
+	self.assertEqual(check_if_positive_num(-3),
+	"")
+	self.assertEqual(check_if_positive_num(27), "")
+
+# def test_truthy_or_falsy(value):
+# 	assert truthy_or_falsy(0b110) == 'Truthy'
+# 	
+
+# def test_odd_or_even():
+# 	a, b, c, d, e = 0, 7, -8, 100, -99
+# 	assert OddOrEven(a) == 'Even'
+# 	assert OddOrEven(b) == 'Odd'
+# 	assert OddOrEven(c) == 'Even'	
+# 	assert OddOrEven(d) == 'Even'	
+# 	assert OddOrEven(e) == 'Odd'	
+
+# def test_smallest_num():
+# 	x, y = 0b1101, -900
+# 	a, b = 0xef40, -0b111
+# 	assert smallestNum(2, -1) == 'The smallest number is -1'
+# 	assert smallestNum(2, 3) == 'The smallest number is 2'
+# 	assert smallestNum(0b111, -0xef4) == 'The smallest number is -3828'
+# 	assert smallestNum(0b110, 55) == 'The smallest number is 6'
